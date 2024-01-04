f2a656150da5eafdc29df33500a56d7757ea2795
CoworkingIQPLUS <coworking@iqplus.no>
2024-01-04 15:25:57 +0100

    style: new header styling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,11 @@ const tailwindPlugin = require('./plugins/tailwind-plugin.cjs');
  * @param {string} section
  * @param {import('@docusaurus/plugin-content-docs').Options} options
  */
-function defineSection(section, version = { label: 'Default Version' }, options = {}) {
+function defineSection(
+  section,
+  version = { label: 'Default Version' },
+  options = {}
+) {
   return [
     '@docusaurus/plugin-content-docs',
     /** @type {import('@docusaurus/plugin-content-docs').Options} */
@@ -44,7 +48,7 @@ function defineSection(section, version = { label: 'Default Version' }, options 
 }
 
 const SECTIONS = [
-  defineSection('sdk', { version: '1.2.1', label: '1.2.1', isLast: true, }),
+  defineSection('sdk', { version: '1.2.1', label: '1.2.1', isLast: true }),
   defineSection('shopify'),
   defineSection('woocommerce'),
   defineSection('wordpress'),
@@ -112,6 +116,8 @@ const config = {
           customCss: [
             require.resolve('./src/css/custom.css'),
             require.resolve('./src/css/api-reference.css'),
+            require.resolve('./src/css/custom-reachu-navbar.css'),
+            require.resolve('./src/css/custom-reachu-docu.css'),
           ],
         },
         googleAnalytics: {
@@ -159,8 +165,8 @@ const config = {
           {
             type: 'docsVersionDropdown',
             position: 'right',
-            label: 'Version',  // puedes cambiar esto al nombre de tu versión actual
-            to: '/',  // este es el enlace predeterminado, que generalmente apunta a la documentación más reciente
+            label: 'Version', // puedes cambiar esto al nombre de tu versión actual
+            to: '/', // este es el enlace predeterminado, que generalmente apunta a la documentación más reciente
           },
           {
             label: 'SDK',
